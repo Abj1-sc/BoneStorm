d202b135582fd85f26d31d6f603cd4a7692c13e4
Abj1-sc <aybandop@ucsc.edu>
2024-03-07 23:28:58 -0800

    Added Lava animation

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -15,7 +15,18 @@ class Load extends Phaser.Scene {
         this.load.image('DOWN', 'DOWN.png')
         this.load.image('LEFT', 'LEFT.png')
         this.load.image('RIGHT', 'RIGHT.png')
-        this.load.image('stage', 'fightStage.png')
+        this.load.image('fly', 'fly.png')
+
+        this.load.spritesheet('stage','FightStage.png', {
+            frameWidth: 1956, 
+            frameHeight: 1436
+        })
+
+        this.load.spritesheet('lava','lava.png', {
+            frameWidth: 1904, 
+            frameHeight: 1420
+        })
+
 
         //this.load.path = './assets/tilemaps/'
 
@@ -32,6 +43,22 @@ class Load extends Phaser.Scene {
 
     create() {
         // animation definitions
+
+        this.anims.create({
+            key: 'stageLava',
+            frameRate: 2,
+            repeat: -1,
+            repeatDelay: 3000,
+            frames: this.anims.generateFrameNumbers('stage', { frames: [ 0, 1, 0]} )
+        })
+
+        this.anims.create({
+            key: 'fallingLava',
+            frameRate: 2,
+            repeat: -1,
+            repeatDelay: 3000,
+            frames: this.anims.generateFrameNumbers('lava', { frames: [ 0, 1, 2, 0]} )
+        })
 
         this.scene.start('sceneKeys')
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,10 +8,33 @@ class Play extends Phaser.Scene {
         this.KEYS = this.scene.get('sceneKeys').KEYS
 
         // screen
-        let screen = this.add.sprite(0, 0, 'screen').setOrigin(0).setDepth(1).setScale(1.5)
+        let screen = this.add.sprite(0, 0, 'screen').setOrigin(0).setDepth(2).setScale(1.5)
 
-        // background
-        let stage = this.add.sprite(0, 0, 'stage').setOrigin(0).setDepth(-2).setScale(.51)
+        // background, lava: #9e1502
+        let stage = this.add.sprite(0, 0, 'stage', 0).setOrigin(0).setDepth(-2).setScale(.51)
+
+        stage.anims.play('stageLava')
+
+        // falling lava
+        let lava = this.add.sprite(0, 0, 'lava', 0).setOrigin(0).setDepth(1).setScale(.51)
+
+        this.time.delayedCall(1000, () => {
+            lava.anims.play('fallingLava')
+        })
+
+        // // flies
+        // let fly1 = this.add.sprite(this.game.width/5, -300, 'fly').setOrigin(0.5).setDepth(1)
+        // let fly2 = this.add.sprite(this.game.width*2/5, -300, 'fly').setOrigin(0.5).setDepth(1)
+        // let fly3 = this.add.sprite(this.game.width*3/5, -300, 'fly').setOrigin(0.5).setDepth(1)
+        // let fly4 = this.add.sprite(this.game.width*4/5, -300, 'fly').setOrigin(0.5).setDepth(1)
+
+        // let flyFall = this.tweens.add({
+        //     targets: [fly1, fly3],
+        //     y: {from: -300, to: this.game.height/2},
+        //     repeat: -1,
+        // })
+
+
 
         // Fight Message
         let title = this.add.bitmapText(game.config.width/2, -300, 'title-font', 'FIGHT!!!!!!', 80).setOrigin(.5)
